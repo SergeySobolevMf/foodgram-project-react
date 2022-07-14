d9bab6927e02e63c2e029097a4fa6a12c92811e8
Сергей Соболев <sergey.sobolev@megafon.ru>
2022-07-14 21:56:50 +0300

    добавлен djoser и логика с рецптами

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -86,32 +86,3 @@ class RecipeList(viewsets.ModelViewSet):
     def del_from_shopping_cart(self, request, pk=None):
         return self.delete_obj(ShoppingList, request.user, pk)
 
-    @action(
-        detail=False, methods=['get'], permission_classes=[IsAuthenticated])
-    def download_shopping_cart(self, request):
-        user = get_object_or_404(CustomUser, username=request.user.username)
-        shopping_cart = user.cart.all()
-        shopping_dict = {}
-        for num in shopping_cart:
-            ingredients_queryset = num.recipe.ingredient.all()
-            for ingredient in ingredients_queryset:
-                name = ingredient.ingredients.name
-                amount = ingredient.amount
-                measurement_unit = ingredient.ingredients.measurement_unit
-                if name not in shopping_dict:
-                    shopping_dict[name] = {
-                        'measurement_unit': measurement_unit,
-                        'amount': amount}
-                else:
-                    shopping_dict[name]['amount'] = (
-                        shopping_dict[name]['amount'] + amount)
-
-        shopping_list = []
-        for index, key in enumerate(shopping_dict, start=1):
-            shopping_list.append(
-                f'{index}. {key} - {shopping_dict[key]["amount"]} '
-                f'{shopping_dict[key]["measurement_unit"]}\n')
-        filename = 'shopping_cart.txt'
-        response = HttpResponse(shopping_list, content_type='text/plain')
-        response['Content-Disposition'] = f'attachment; filename={filename}'
-        return response
